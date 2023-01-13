b0968be2b009a73512a82f40c086e1bd393f57c0
Luke Zhou <sharlock@gmail.com>
2023-01-13 20:48:20 +1100

    create root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'games/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "games#index"
 end
